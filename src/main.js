d6ee4c1491413cdb837b92f0cc5cce69091fa51a
Gabriel Nunes <prismaticnetwork12@gmail.com>
2021-05-22 11:10:07 -0300

    [feat] VueRouter was added

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { BootstrapVue } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import VueRouter from 'vue-router';
 
 Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
