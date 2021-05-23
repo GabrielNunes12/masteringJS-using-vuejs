225d093f9e1cbc26563aa390e2205d70d9856e80
Gabriel Nunes <prismaticnetwork12@gmail.com>
2021-05-23 18:54:42 -0300

    [feat] section about created

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,18 @@ export const config = {
   faq: 'FAQs',
   about: 'About'
 }
+
+export const pathImgObj = {
+  facebook: {
+    src: `./assets/img/facebook.png`,
+    width: '30px',
+    height: '30px',
+    href: 'https://www.facebook.com/gabriel.nunes.148116/'
+  },
+  github: {
+    src: `./assets/img/github.png`,
+    height: '40px',
+    width: '40px',
+    href: 'https://github.com/GabrielNunes12'
+  }
+}
