7d035ae3146f9f88034e8445dbacfe4f85b26447
Gabriel Nunes <prismaticnetwork12@gmail.com>
2021-05-30 19:52:57 -0300

    [fix] test

--- __tests__/Home.spec.js
+++ __tests__/Home.spec.js
@@ -1,5 +1,19 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Home from '../src/views/components/Home.vue'
+
 describe('Home test', () => {
-  test('setup correctly', () => {
-    expect(true).toBe(true);
+  it('should return new anime added', () => {
+    const animeArray = [
+      {value: 'aaa', header: 'lulu', title: 'zuldsdsd'}
+    ]
+    const localVue = createLocalVue();
+    const wrapper = shallowMount(Home, {
+      localVue,
+      propsData: {
+        animeArray
+      }
+    });
+    wrapper.vm.addSomeAnimes();
+    expect(animeArray[0].value.toText()).toBe('aaa');
   })
 })
